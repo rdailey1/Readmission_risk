6d528f7599c7abd6e7b3909e78edcf619bf98975
Ryan <crdailey01@gmail.com>
2026-05-12 08:51:35 -0700

    Update model evaluation metrics

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from preprocessing import preprocess_data
-from model_training import train_models
+from model_training import train_models, evaluate_models
 
 processed_df = preprocess_data()
 
 logistic_model, random_forest_model, X_train, X_test, y_train, y_test = train_models(
     processed_df
 )
-print("Preprocessing and model training complete from main.")
+evaluate_models(
+    logistic_model,
+    random_forest_model,
+    X_test,
+    y_test
+)
+
+print("Preprocessing, training, and evaluation complete from main.")
--- model_training.py
+++ model_training.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 def build_models():
 
@@ -41,3 +42,30 @@ def train_models(processed_df):
     print("Trained Random Forest model")
 
     return logistic_model, random_forest_model, X_train, X_test, y_train, y_test
+
+
+def evaluate_models(logistic_model, random_forest_model, X_test, y_test):
+
+    models = {
+        "Logistic Regression": logistic_model,
+        "Random Forest": random_forest_model
+    }
+
+    for model_name, model in models.items():
+
+        predictions = model.predict(X_test)
+
+        # % of correct predictions
+        accuracy = accuracy_score(y_test, predictions)
+        # how many of predicted + were + (true positives)
+        precision = precision_score(y_test, predictions)
+        # of all +, how many were found
+        recall = recall_score(y_test, predictions)
+        # balance of precision and recall
+        f1 = f1_score(y_test, predictions)
+
+        print(f"\n{model_name} Evaluation")
+        print("Accuracy:", accuracy)
+        print("Precision:", precision)
+        print("Recall:", recall)
+        print("F1 Score:", f1)
